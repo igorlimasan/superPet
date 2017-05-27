f3510eb9a82156bc081a4a4b677cf88e1a61393f
João Vitor Lessa <jvcadastro@gmail.com>
2017-05-27 19:02:06 -0300

    Alterações SASS
    Alterações SASS

--- Aplicacao/src/app/landing-page/landing-page.component.ts
+++ Aplicacao/src/app/landing-page/landing-page.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-landing-page',
-  templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.css']
+  templateUrl: './landing-page.component.html'
 })
 export class LandingPageComponent implements OnInit {
 
